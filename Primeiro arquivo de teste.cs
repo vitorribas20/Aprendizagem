54dcb3d54dceb048d08f379f7f3f6fd394e60f06
vitorribas20 <54421968+vitorribas20@users.noreply.github.com>
2021-08-11 21:02:43 -0300

    Este é o primeiro arquivo só pra testar mesmo
    Dia 11/08/21
    
    e lá vamos nós

---
+++ Primeiro arquivo de teste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Opa, beleza?, o comando read line serve para ler, ou seja, você coloca o write e dps o realline, o read line fica esperando o usuário pressionar enter");
+            Console.WriteLine("pra quebrar linha tem que por o writeline");
+            Console.WriteLine("para começar um comando de texto, por alguma coisa na tela vai ficar assim, ConsoleWriteLine");
+            Console.WriteLine("a estrutura de criação é essa do texto, bem de boa, paramos em 1 hora de vídeo, até o momento bem tranquilo");
+            Console.WriteLine("DIa 11/08/21");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
